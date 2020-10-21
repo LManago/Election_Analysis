130493ba3160edcc8cb4d66f4cf76501da3284d7
LManago <71472813+LManago@users.noreply.github.com>
2020-10-20 23:53:37 -0400

    updating code for election results

--- PyPoll_Challenge.py
+++ PyPoll_Challenge.py
@@ -28,8 +28,10 @@ winning_percentage = 0
 
 # 2: Track the largest county and county voter turnout.
 largest_county = ""
+cwinning_county = ""
 largest_count = 0
 largest_percentage = 0
+county_vote = 0
 
 # Read the csv and convert it into a list of dictionaries
 with open(file_to_load) as election_data:
@@ -42,7 +44,7 @@ with open(file_to_load) as election_data:
     for row in reader:
 
         # Add to the total vote count
-        total_votes = total_votes + 1
+        total_votes += 1
 
         # Get the candidate name from each row.
         candidate_name = row[2]
@@ -85,7 +87,7 @@ with open(r'C:\Users\Lakenia\Desktop\Election_Analysis\Resources\Election_result
         f"\nElection Results\n"
         f"-------------------------\n"
         f"Total Votes: {total_votes:,}\n"
-        f"-------------------------\n\n"
+        f"-------------------------\n"
         f"Count Votes:\n")
     print(election_results, end="")
 
@@ -94,24 +96,24 @@ with open(r'C:\Users\Lakenia\Desktop\Election_Analysis\Resources\Election_result
     # 6a: Write a repetition statement to get the county from the county dictionary.
     for county in county_votes:
         # 6b: Retrieve the county vote count.
-        votes = county_votes[county]
+        cvotes = county_votes[county]
         # 6c: Calculate the percent of total votes for the county.
-        vote_percentage = float(votes)/ float(total_votes) * 100
+        cvote_percentage = int(cvotes)/ int(total_votes) * 100
 
          # 6d: Print the county results to the terminal.
-        voter_output = f"{county}: {vote_percentage:.3f}% ({county_votes})\n"
-        print(voter_output, end="")
+        county_output = f"{county}: {cvote_percentage:.3f}% ({county_votes})\n"
+        print(county_output)
          # 6e: Save the county votes to a text file.
-        txt_file.write(voter_output)
+        txt_file.write(county_output)
          # 6f: Write a decision statement to determine the winning county and get its vote count.
-        if (votes>winning_count):
-            winning_count = votes
-            winning_county = county
+        if (cvotes > largest_count):
+            largest_count = cvotes
+            cwinning_county = county
 
     # 7: Print the county with the largest turnout to the terminal.
     largest_county_summary = (    
         f"---------------------------\n"
-        f"County with largest turnout: {winning_county}\n"
+        f"County with largest turnout: {cwinning_county}\n"
         f"---------------------------\n")    
 
     print(largest_county_summary)
@@ -124,7 +126,7 @@ with open(r'C:\Users\Lakenia\Desktop\Election_Analysis\Resources\Election_result
 
         # Retrieve vote count and percentage
         votes = candidate_votes.get(candidate_name)
-        vote_percentage = float(votes) / float(total_votes) * 100
+        vote_percentage = float(votes)/ float(total_votes) * 100
         candidate_results = (
             f"{candidate_name}: {vote_percentage:.1f}% ({votes:,})\n")
 
@@ -136,7 +138,7 @@ with open(r'C:\Users\Lakenia\Desktop\Election_Analysis\Resources\Election_result
 
         # Determine winning vote count, winning percentage, and candidate.
         if (votes > winning_count) and (vote_percentage > winning_percentage):
-            winning_count = candidate_votes
+            winning_count = votes
             winning_candidate = candidate_name
             winning_percentage = vote_percentage
 
